def1352e4bd590b54bfffbdbb1e5cd9dffe6a5d8
Davor Telisman <davortelisman@gmail.com>
2023-12-21 08:39:58 +0100

    create user login function in view.py

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .registration_form import UserRegistrationForm
-
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 def register_user(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
@@ -13,3 +14,18 @@ def register_user(request):
         form = UserRegistrationForm()
     return render(request, 'register.html', {'form': form})
 
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Redirect to a specific page after login
+                return redirect('')  # Replace 'home' with your desired URL name
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
