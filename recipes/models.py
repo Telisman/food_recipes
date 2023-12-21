b66c72dec968e8a7dcffb0ad3b04102c0e265883
Davor Telisman <davortelisman@gmail.com>
2023-12-21 08:06:59 +0100

    Set up models for are projects and add user to settings AUTH_USER_MODEL = 'user.User'

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from user.models import User
+from ingredient.models import Ingredient
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    recipe_text = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
