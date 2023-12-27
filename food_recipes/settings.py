5c0f7899cb6994eca4a85615f9a13068ce5d072d
Davor Telisman <davortelisman@gmail.com>
2023-12-27 10:12:45 +0100

    docker

--- food_recipes/settings.py
+++ food_recipes/settings.py
@@ -34,6 +34,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
 ]
 
 ROOT_URLCONF = "food_recipes.urls"
